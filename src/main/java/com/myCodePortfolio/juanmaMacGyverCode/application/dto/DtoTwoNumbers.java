20d952dd4b5513c49201cc582cd188dcbaba1970
Juanma <jmlpdr.93@gmail.com>
2021-02-02 16:56:37 +0000

    :wrench: Created lombok config

--- src/main/java/com/myCodePortfolio/juanmaMacGyverCode/application/dto/DtoTwoNumbers.java
+++ src/main/java/com/myCodePortfolio/juanmaMacGyverCode/application/dto/DtoTwoNumbers.java
@@ -3,7 +3,6 @@ package com.myCodePortfolio.juanmaMacGyverCode.application.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Generated;
 
 @Data
 @Builder
